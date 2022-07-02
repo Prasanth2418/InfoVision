dd0544fa04034b8e8afd8a2aa38305393a96d2f6
Chaitanya Rayana <chaitanyachaituu123@gmail.com>
2022-07-02 12:43:02 +0530

    update

--- src/Components/Common/NavBar.js
+++ src/Components/Common/NavBar.js
@@ -9,26 +9,24 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 import Avatar from "@mui/material/Avatar";
 
-
-
 const StyledBadge = styled(Badge)(({ theme }) => ({
   "& .MuiBadge-badge": {
     right: -3,
-   top: 13,
-   border: `2px solid ${theme.palette.background.paper}`,
-   padding: "0 4px",
+    top: 13,
+    border: `2px solid ${theme.palette.background.paper}`,
+    padding: "0 4px",
   },
 }));
 const NavBar = () => {
   const Navigate = useNavigate();
-  const [search,setsearch]=useState()
-  
+  const [search, setsearch] = useState();
+
   return (
     <div className="header">
       <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container-fluid">
-          <a class="navbar-brand" onClick={() => Navigate("/")}>
-            <img src={Logo} className="logo"/>
+          <a class="navbar-brand" onClick={() => Navigate("/home")}>
+            <img src={Logo} className="logo" />
           </a>
           <button
             class="navbar-toggler"
@@ -79,20 +77,30 @@ const NavBar = () => {
                   Dinner
                 </button>
               </li>
-            </ul> &nbsp; &nbsp; &nbsp; &nbsp;
+            </ul>{" "}
+            &nbsp; &nbsp; &nbsp; &nbsp;
             <form class="nosubmit">
-  <input class="nosubmit" type="search" placeholder="Search for Items..." value={search}/>
-</form>
-            </div>
-           
+              <input
+                class="nosubmit"
+                type="search"
+                placeholder="Search for Items..."
+                value={search}
+              />
+            </form>
+          </div>
         </div>
-        <IconButton className="cart" onClick={()=>Navigate("/cart")}>
-              <StyledBadge badgeContent={1} color="secondary">
-                <ShoppingCartIcon />
-              </StyledBadge>
-            </IconButton> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <Avatar alt="profile" className="profile" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZSUyMHNob3R8ZW58MHx8MHx8&w=1000&q=80" />
-            &nbsp; &nbsp; &nbsp;
+        <IconButton className="cart" onClick={() => Navigate("/cart")}>
+          <StyledBadge badgeContent={1} color="secondary">
+            <ShoppingCartIcon />
+          </StyledBadge>
+        </IconButton>{" "}
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <Avatar
+          alt="profile"
+          className="profile"
+          src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZSUyMHNob3R8ZW58MHx8MHx8&w=1000&q=80"
+        />
+        &nbsp; &nbsp; &nbsp;
       </nav>
     </div>
   );
