a9bc15f36ce26949f6eb8879b6d00a4529e48b3f
Chaitanya Rayana <chaitanyachaituu123@gmail.com>
2022-07-02 21:33:41 +0530

    update

--- src/Pages/Payment/PaymentPage.js
+++ src/Pages/Payment/PaymentPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import StripeCheckout from "react-stripe-checkout";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
@@ -11,10 +11,28 @@ export const PaymentPage = () => {
 
     price: 50,
   });
+  const [data, setData] = useState([]);
+
+  const [total, setTotal] = useState();
+  //   let dispatch = useDispatch();
+  useEffect(() => {
+    setTotal(
+      data.reduce(
+        (acc, curr) =>
+          acc + Number(curr.restaurantItem.itemprice) * curr.quantity,
+        0
+      )
+    );
+  }, [data]);
+  useEffect(() => {
+    axios
+      .get(
+        "https://backend123.azurewebsites.net/infovision/cart?cartId=dec5ac08-14c2-4d61-bf6a-fc12eaa2597f"
+      )
+      .then((res) => setData(res.data));
+  }, []);
 
   async function handleToken(token) {
-   
-
     const response = await axios
       .get("https://ssid5w.sse.codesandbox.io/")
 
@@ -29,23 +47,16 @@ export const PaymentPage = () => {
 
   return (
     <div className="product23">
-     
-
       <StripeCheckout
         stripeKey="pk_test_51KizaRSG6dDLlcjJWj1gLxdztzdK41Su8hei0gbgeXslyFnJYx5prvSYDY0YORFi12WMxprnKhK19GOrtmmBH08m00ehulGL5J"
-       
-
         token={handleToken}
         billingAddress
         shippingAddress
-        amount={product.price}
+        amount={(total + 30) * 100}
         name={product.name}
-        
       />
     </div>
   );
 };
 
 export default PaymentPage;
-
-
